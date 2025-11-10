44d3204e36340bdeeb0a892b880256130784917a
bengeendokter <benifit.arts@gmail.com>
2025-11-10 22:15:07 +0100

    Add title types

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,40 @@ interface SetCard {
     setName: string;
 }
 
-type Type = 'Grass' | 'Fire' | 'Water' | 'Lightning' | 'Psychic' | 'Darkness' | 'Metal' | 'Fairy';
-type EnergyCard = `Basic_${Type}_Energy`;
+const ENERGY_TYPES = {
+    GRASS: 'Grass',
+    FIRE: 'Fire',
+    WATER: 'Water',
+    LIGHTNING: 'Lightning',
+    PSYCHIC: 'Psychic',
+    DARKNESS: 'Darkness',
+    METAL: 'Metal',
+    FAIRY: 'Fairy',
+} as const satisfies Record<Uppercase<string>, string>;
+
+type EnergyType = typeof ENERGY_TYPES[keyof typeof ENERGY_TYPES];
+type EnergyTitle = `Basic ${EnergyType} Energy (TCG)`;
+
+function isEnergyTitle(text: string): text is EnergyTitle {
+    return Object.values(ENERGY_TYPES)
+        .map((energyType): EnergyTitle => `Basic ${energyType} Energy (TCG)`)
+        .some(energyTitle => {
+            return text === energyTitle;
+        });
+}
 
 interface CardWithQuantity {
-    card: SetCard | EnergyCard;
+    card: SetCard | EnergyType;
     quantity: number;
 }
 
-interface Deck {
-    cards: CardWithQuantity[];
+type CardTitle = `${string} (${string} ${number})`;
+
+function isCardTitle(text: string): text is CardTitle {
+    return /.+ \(.+ \d+\)$/.test(text);
 }
+
+type QuantityText = `${number}×`;
 
 async function main(): Promise<void> {
     const url: string = 'https://bulbapedia.bulbagarden.net/wiki/Mega_Gengar_ex_Mega_Battle_Deck_(TCG)';
@@ -30,7 +53,28 @@ async function main(): Promise<void> {
         return;
     }
 
-    console.log(table.outerHTML);
+    const deck: CardWithQuantity[] = [];
+
+    const rows: NodeListOf<Element> = table.querySelectorAll('tr:has(td:nth-of-type(3))');
+
+    rows.forEach(row => {
+        const cardTableData: Element | null = row.querySelector('td:nth-of-type(3) a');
+        const quantityTableData: Element | null = row.querySelector('td:nth-of-type(4)');
+
+        if (!cardTableData || !quantityTableData) {
+            return;
+        }
+
+        const title: string | null = cardTableData.getAttribute('title');
+        const quantityText: string = quantityTableData.textContent.trim();
+
+        if (!title) {
+            return;
+        }
+
+        console.log(isCardTitle(title) || isEnergyTitle(title));
+        console.log(quantityText);
+    });
 }
 
 await main();
