7480082ab19408e01c7d15f1b377fa10c934ab22
bengeendokter <benifit.arts@gmail.com>
2025-11-10 20:21:19 +0100

    Get dom table

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-const name: string = "World";
+import { JSDOM } from 'jsdom';
 
-console.log("Hello, " + name);
+async function main(): Promise<void> {
+    const url: string = 'https://bulbapedia.bulbagarden.net/wiki/Mega_Gengar_ex_Mega_Battle_Deck_(TCG)';
+    const pageText: string = await fetch(url).then(res => res.text());
+    const dom: JSDOM = new JSDOM(pageText);
+
+    const table: Element | null = dom.window.document.querySelector('h2:has(#Deck_list) + table');
+
+    if (!table) {
+        console.log('Deck list table not found');
+        return;
+    }
+
+    console.log(table.outerHTML);
+}
+
+await main();
