b72efb2f76acbc6f0ad02c1365c6eaa82731b5c2
bengeendokter <benifit.arts@gmail.com>
2025-11-12 18:22:28 +0100

    Create exportDecksToLimitless

---
+++ src/prebuild/data-access/export-decks-to-limitless.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import type { Deck } from '../model/prebuild-deck.ts';
+
+export interface ExportDecksToLimitlessParams {
+	decks: Deck[];
+	outputDirectory: string;
+}
+
+export function exportDecksToLimitless({
+	decks,
+	outputDirectory,
+}: ExportDecksToLimitlessParams): void {
+	decks.forEach((deck) => {
+		const deckFileName = `${outputDirectory}/${deck.name}.txt`;
+		// TODO implement limitless export format when missing data is available
+		// fs.writeFileSync(deckFileName, JSON.stringify(deck, null, 2), {
+		// 	encoding: 'utf-8',
+		// });
+	});
+}
