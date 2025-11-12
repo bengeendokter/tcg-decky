9cb054017fcc8273b96a24f4d803b334300588c2
bengeendokter <benifit.arts@gmail.com>
2025-11-12 19:08:52 +0100

    Add convertPrebuildToLimitlessDeck

---
+++ src/limitless/feature/convert-prebuild-to-limitless-deck.ts
@@ -0,0 +1,8 @@
+import type { PrebuildDeck } from '../../prebuild/model/prebuild-deck.ts';
+import type { LimitlessDeck } from '../model/limitless-deck.ts';
+
+export async function convertPrebuildToLimitlessDeck(
+	prebuildDeck: PrebuildDeck,
+): Promise<LimitlessDeck> {
+	return undefined;
+}
