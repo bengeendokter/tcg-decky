320aa32ccf5aa7130a28e6f59260e03fbb2317bd
bengeendokter <benifit.arts@gmail.com>
2025-11-13 08:36:19 +0100

    Get more data

--- src/limitless/feature/convert-prebuild-to-limitless-deck.ts
+++ src/limitless/feature/convert-prebuild-to-limitless-deck.ts
@@ -1,6 +1,11 @@
 import type { PrebuildDeck } from '../../prebuild/model/prebuild-deck.ts';
 import type { LimitlessCard, LimitlessDeck } from '../model/limitless-deck.ts';
-import TCGdex, { Query, type Card, type CardResume } from '@tcgdex/sdk';
+import TCGdex, {
+	Query,
+	type Card,
+	type CardResume,
+	type Set,
+} from '@tcgdex/sdk';
 
 const tcgdex = new TCGdex('en');
 
@@ -8,12 +13,39 @@ export async function convertPrebuildToLimitlessDeck({
 	name,
 	cards,
 }: PrebuildDeck): Promise<LimitlessDeck> {
-	const card: CardResume[] = await tcgdex.card.list(
+	// TODO filter out energies
+
+	const cardResumes: CardResume[] = await tcgdex.card.list(
 		Query.create().like('localId', '136').like('set.name', 'Darkness Ablaze'),
 	);
 
-	console.log('length', card.length);
-	console.log('name', card[0]?.name);
+	const cardResume: CardResume | undefined = cardResumes[0];
+
+	if (!cardResume) {
+		throw Error('Card not found');
+	}
+
+	const cardId: string = cardResume.id;
+
+	const card: Card | null = await tcgdex.card.get(cardId);
+
+	if (!card) {
+		throw Error('Card not found');
+	}
+
+	const category: string = card.category;
+
+	const set: Set | null = await tcgdex.set.get(card.set.id);
+
+	if (!set) {
+		throw Error('Set not found');
+	}
+
+	const tcgOnline: string | undefined = set.tcgOnline;
+
+	if (!tcgOnline) {
+		throw Error('Set TCG Online code not found');
+	}
 
 	const pokemon: LimitlessCard[] = [];
 	const trainer: LimitlessCard[] = [];
