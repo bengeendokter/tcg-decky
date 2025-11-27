9e4c54cd9c455951fbc4d2c30c61250fe3b6cf06
bengeendokter <benifit.arts@gmail.com>
2025-11-27 21:24:25 +0100

    Implement cards api

--- src/apps/api/cards.ts
+++ src/apps/api/cards.ts
@@ -1,3 +1,10 @@
 import { Hono } from 'hono';
+import { type CollectionCard } from '../../libs/collection/model/collection-card.ts';
+import { getAllCollectionCards } from '../../libs/collection/data-access/get-all-collection-cards.ts';
+import { db } from './hono.ts';
 
-export const cards = new Hono().get('/', (c) => c.json('list authors'));
+export const cards = new Hono().get('/', async (context) => {
+	const cards: CollectionCard[] = await getAllCollectionCards(db);
+
+	return context.json(cards);
+});
--- src/apps/api/hono.ts
+++ src/apps/api/hono.ts
@@ -2,8 +2,13 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cards } from './cards.ts';
 import { logger } from 'hono/logger';
+import { connectToDatabase } from '../../libs/collection/data-access/connect-to-database.ts';
+import type { Db } from 'mongodb';
+import { CONFIG } from '../../config.ts';
 
 export const app = new Hono();
+
+export const db: Db = await connectToDatabase(CONFIG.MONGO_DB_DATABASE_URL);
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!');
@@ -16,7 +21,7 @@ const routes = app.route('/cards', cards);
 serve(
 	{
 		fetch: app.fetch,
-		port: 3000,
+		port: 4000,
 	},
 	(info) => {
 		console.log(`Server is running on http://localhost:${info.port}`);
