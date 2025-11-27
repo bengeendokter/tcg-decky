4a9dda0af14dc1fdd200f9f4ba30164715f97d87
bengeendokter <benifit.arts@gmail.com>
2025-11-27 19:45:34 +0100

    Add comments

--- src/hono.ts
+++ src/hono.ts
@@ -1,15 +1,30 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+// - api/cards
+// - GET
+
+// - api/decks
+// - GET
+
+// 	- api/decks/:id
+// 	- GET
+// 	- POST
+// 	- UPDATE
+// 	- DELETE
+
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+	return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
+);
