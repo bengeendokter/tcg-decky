57d4fccdbe7928329c3fa73b4b626d1aec150dfe
bengeendokter <benifit.arts@gmail.com>
2025-12-02 20:43:20 +0100

    Update config

--- src/libs/deck-builder/data-access/collection.ts
+++ src/libs/deck-builder/data-access/collection.ts
@@ -2,7 +2,7 @@ import { Injectable, resource, type ResourceRef } from '@angular/core';
 import { hc } from 'hono/client';
 import type { AppType } from '../../../apps/collection-api/hono.ts';
 import type { CollectionCard } from '../../collection/model/collection-card.ts';
-import { CONFIG } from '../../../apps/deck-builder/environment/environment.ts';
+import { CONFIG } from '../../../environment/environment.ts';
 
 @Injectable({ providedIn: 'root' })
 export class Collection {
