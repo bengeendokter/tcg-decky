2aff3d8970ef4d6061b73496ae0c7efa3da7c4b5
bengeendokter <benifit.arts@gmail.com>
2025-12-02 21:27:32 +0100

    Fix typescript error

--- src/apps/scripts/extract-prebuild-decks-to-json.ts
+++ src/apps/scripts/extract-prebuild-decks-to-json.ts
@@ -1,7 +1,7 @@
-import { CONFIG } from './config.ts';
-import { exportPrebuildDecksToJson } from './prebuild/data-access/export-prebuild-decks-to-json.ts';
-import { extractPrebuildDecks } from './prebuild/feature/extract-prebuild-deck.ts';
-import type { PrebuildDeck } from './prebuild/model/prebuild-deck.ts';
+import { CONFIG } from '../../config.ts';
+import { exportPrebuildDecksToJson } from '../../libs/prebuild/data-access/export-prebuild-decks-to-json.ts';
+import { extractPrebuildDecks } from '../../libs/prebuild/feature/extract-prebuild-deck.ts';
+import type { PrebuildDeck } from '../../libs/prebuild/model/prebuild-deck.ts';
 
 export async function extractPrebuildDecksToJson(): Promise<void> {
 	const outputDirectory: string = CONFIG.COLLECTION_OUTPUT_DIRECTORY;
