6f1e6c40258c5ae1a7843bb8163b6ae7dd8409c9
bengeendokter <benifit.arts@gmail.com>
2025-12-03 18:50:18 +0100

    Update layout

--- src/apps/deck-builder/app/app.ts
+++ src/apps/deck-builder/app/app.ts
@@ -48,4 +48,14 @@ export class App {
 	protected async getCard(id: string): Promise<void> {
 		this.cardId.set(id);
 	}
+
+	protected getQuantitySum(variants: CollectionCard['variants']): number {
+		const firstEdition: number = variants.firstEdition ?? 0;
+		const holo: number = variants.holo ?? 0;
+		const normal: number = variants.normal ?? 0;
+		const reverse: number = variants.reverse ?? 0;
+		const wPromo: number = variants.wPromo ?? 0;
+
+		return firstEdition + holo + normal + reverse + wPromo;
+	}
 }
