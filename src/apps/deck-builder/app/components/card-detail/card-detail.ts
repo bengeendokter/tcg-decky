086ebbc647e10778d6180835cb8f9f0d4f8fd24a
bengeendokter <benifit.arts@gmail.com>
2026-03-14 10:29:45 +0100

    Update styles

--- src/apps/deck-builder/app/components/card-detail/card-detail.ts
+++ src/apps/deck-builder/app/components/card-detail/card-detail.ts
@@ -11,10 +11,13 @@ import {
 import { TcgCard } from '../tcg-card/tcg-card';
 import type { DeckCard } from '../../../../../libs/deck-builder/model/deck-card';
 import { getQuantitySum } from '../../../../../libs/deck-builder/util/get-quantity-sum';
+import { IconComponent } from '../icon/icon.component';
+import addIcon from '@material-symbols/svg-400/rounded/add.svg';
+import removeIcon from '@material-symbols/svg-400/rounded/remove.svg';
 
 @Component({
 	selector: 'card-detail',
-	imports: [TcgCard],
+	imports: [TcgCard, IconComponent],
 	templateUrl: './card-detail.html',
 	styleUrl: './card-detail.css',
 })
@@ -25,6 +28,8 @@ export class CardDetail {
 	protected readonly getQuantitySum: typeof getQuantitySum = getQuantitySum;
 	public readonly removeCard: OutputEmitterRef<DeckCard> = output();
 	public readonly addCard: OutputEmitterRef<DeckCard> = output();
+	protected readonly addIcon: string = addIcon;
+	protected readonly removeIcon: string = removeIcon;
 
 	public openCardDetail(): void {
 		this.cardDetail().nativeElement.showModal();
