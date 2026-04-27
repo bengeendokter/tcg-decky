19151c956287b33a2a0ec28b73e0f5a1c1e692d5
bengeendokter <benifit.arts@gmail.com>
2026-04-27 22:14:50 +0200

    Add snackbar story

---
+++ src/libs/style/ui/component/md-snackbar.stories.ts
@@ -0,0 +1,10 @@
+import { html } from "lit";
+import preview from "#.storybook/preview";
+
+const meta = preview.meta({
+  title: "Snackbar",
+});
+
+export const Primary = meta.story({
+  render: () => html`<output class="md-snackbar">Snackbar without action</output>`,
+});
