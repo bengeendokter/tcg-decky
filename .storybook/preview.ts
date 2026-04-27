11ca1ea4b75cc5311528465bb0d0e5c983e8ccaf
bengeendokter <benifit.arts@gmail.com>
2026-04-27 19:42:55 +0200

    Update config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,10 @@
 import { definePreview } from "@storybook/web-components-vite";
 import addonA11y from "@storybook/addon-a11y";
-// import addonVitest from "@storybook/addon-vitest";
 import addonDocs from "@storybook/addon-docs";
 
 export default definePreview({
   addons: [addonA11y(), addonDocs()],
+	tags: ['autodocs'],
   parameters: {
     a11y: {
       options: { xpath: true },
