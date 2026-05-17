386ba0258b1cc6623cb9b1e2d1d9324649681912
bengeendokter <benifit.arts@gmail.com>
2026-05-17 18:32:51 +0200

    Update storybook

--- src/libs/style/ui/component/md-button.stories.ts
+++ src/libs/style/ui/component/md-button.stories.ts
@@ -18,7 +18,7 @@ export const Text = meta.story({
 });
 
 export const Icon = meta.story({
-	render: () => html`<button class="md-icon-button">
+	render: () => html`<button class="md-icon-button" aria-label="home">
 		<svg height="24px" viewBox="0 -960 960 960" width="24px">
 			<path
 				d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"
@@ -28,7 +28,7 @@ export const Icon = meta.story({
 });
 
 export const TonalIcon = meta.story({
-	render: () => html`<button class="md-tonal-button md-icon-button">
+	render: () => html`<button class="md-tonal-button md-icon-button" aria-label="home">
 		<svg height="24px" viewBox="0 -960 960 960" width="24px">
 			<path
 				d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"
--- src/libs/style/ui/component/md-snackbar.stories.ts
+++ src/libs/style/ui/component/md-snackbar.stories.ts
@@ -3,10 +3,15 @@ import preview from '#.storybook/preview';
 
 const meta = preview.meta({
 	title: 'Components/Snackbar',
+	parameters: {
+		docs: {
+			story: {
+				height: '150px',
+			},
+		},
+	},
 });
 
 export const Primary = meta.story({
-	render: () => html`<div style="min-height: 150px">
-		<output class="md-snackbar">Snackbar without action</output>
-	</div>`,
+	render: () => html`<output class="md-snackbar">Snackbar without action</output>`,
 });
