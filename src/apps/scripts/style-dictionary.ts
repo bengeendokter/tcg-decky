25cb2bfd9dd7308f900b7a5502e01e2a8897b8cf
bengeendokter <benifit.arts@gmail.com>
2026-06-14 14:08:21 +0200

    Correct css output

--- src/apps/scripts/style-dictionary.ts
+++ src/apps/scripts/style-dictionary.ts
@@ -2,14 +2,14 @@ import { darkTheme, getThemeTokens, lightTheme } from '@style/model/theme';
 import { getPaletteTokens } from '@style/feature/export-palette-tokens';
 import StyleDictionary, { type TransformedToken } from 'style-dictionary';
 import {
-	formats,
 	transformGroups,
 	logWarningLevels,
 	logVerbosityLevels,
 	logBrokenReferenceLevels,
+	propertyFormatNames,
 } from 'style-dictionary/enums';
 import type { FormatFn } from 'style-dictionary/types';
-import { fileHeader } from 'style-dictionary/utils';
+import { fileHeader, formattedVariables } from 'style-dictionary/utils';
 import type { Oklch } from '@style/model/palette';
 
 interface GetPaletteCssVariableParams {
@@ -38,10 +38,59 @@ const paletteFormat: FormatFn = async ({ dictionary, file }) => {
 	return header + ['html {', ...allVariables, '}'].join('\n');
 };
 
+const themeFormat: FormatFn = async ({ dictionary, file, options }) => {
+	const header = await fileHeader({ file });
+	const theme: string | undefined = Object.keys(dictionary.tokens)[0];
+
+	if (typeof theme !== 'string') {
+		throw Error('Invalid theme');
+	}
+
+	const mappedNamedictionary = {
+		...dictionary,
+		allTokens: dictionary.allTokens.map((transformedToken) => {
+			const name = transformedToken.name.replace(theme, 'sys-color');
+			return { ...transformedToken, name };
+		}),
+		unfilteredTokens: {
+			...dictionary.unfilteredTokens,
+			palette: Object.fromEntries(
+				Object.entries(dictionary.unfilteredTokens?.['palette'] as Record<string, unknown>).map(
+					([key, value]) => {
+						return [
+							key,
+							Object.fromEntries(
+								Object.entries(value as Record<string, TransformedToken>).map(
+									([key, value]) => {
+										return [key, { ...value, name: 'ref-' + value.name }];
+									},
+								),
+							),
+						];
+					},
+				),
+			),
+		},
+	};
+
+	const allVariables: string = formattedVariables({
+		format: propertyFormatNames.css,
+		dictionary: mappedNamedictionary,
+		outputReferences: options.outputReferences,
+		sort: options.sort,
+		usesDtcg: true,
+		formatting: {
+			prefix: '--md-',
+		},
+	});
+
+	return header + [`.${theme} {`, allVariables, '}'].join('\n');
+};
+
 function generateThemeFiles(themes: string[]) {
 	return themes.map((theme) => ({
 		destination: `${theme}.css`,
-		format: formats.cssVariables,
+		format: 'themeFormat',
 		filter: (token: TransformedToken) => token.path[0] === theme,
 		options: {
 			outputReferences: true,
@@ -70,10 +119,10 @@ const styleDictionary: StyleDictionary = new StyleDictionary({
 		},
 	},
 	log: {
-		warnings: logWarningLevels.warn, // 'warn' | 'error' | 'disabled'
-		verbosity: logVerbosityLevels.verbose, // 'default' | 'silent' | 'verbose'
+		warnings: logWarningLevels.warn,
+		verbosity: logVerbosityLevels.verbose,
 		errors: {
-			brokenReferences: logBrokenReferenceLevels.throw, // 'throw' | 'console'
+			brokenReferences: logBrokenReferenceLevels.throw,
 		},
 	},
 });
@@ -81,6 +130,11 @@ const styleDictionary: StyleDictionary = new StyleDictionary({
 StyleDictionary.registerFormat({
 	name: 'paletteFormat',
 	format: paletteFormat,
+});
+
+StyleDictionary.registerFormat({
+	name: 'themeFormat',
+	format: themeFormat,
 });
 
 await styleDictionary.buildPlatform(transformGroups.css);
